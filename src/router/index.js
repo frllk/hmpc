8e9fd913ebca29be5bc42018e063424d166c1cec
frllk <1402805300@qq.com>
2020-05-22 21:46:28 +0800

    layout基本布局（Container 布局容器）

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/login'
+import Layout from '../views/layout'
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/', component: Layout },
   { path: '/login', component: Login }
 ]
 
---
+++ src/views/layout/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-container class="layout-container">
+    <el-aside class="aside" width="200px">Aside</el-aside>
+    <el-container>
+      <el-header class="header">Header</el-header>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  props: { },
+  data () {
+    return { }
+  },
+  methods: { },
+  computed: { },
+  created () { },
+  mounted () { }
+}
+</script>
+
+<style scoped lang='less'>
+ /* 外层的容器
+    占满整个页面
+  */
+  .layout-container {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+  }
+
+  .aside {
+    background-color: #d3dce6;
+  }
+
+  .header {
+    background-color: #b3c0d1;
+  }
+
+  .main {
+    background-color: #e9eef3;
+  }
+</style>
