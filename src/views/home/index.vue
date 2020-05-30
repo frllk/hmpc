72ca7a362c22dd26167b590cb9745ea285a4a458
frllk <1402805300@qq.com>
2020-05-30 12:59:25 +0800

    封装频道组件 --- 在组件上面使用v-model

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,19 +1,24 @@
 <template>
   <div class=''>
+    <!-- 由于它的根元素是el-form-item，则在使用这个组件时，要用el-form包起来 -->
     <el-form>
       <my-channels @changeChannel="hChangeChannel" :isClearable="true"></my-channels>
+      <!-- 1、使用。在子组件使用v-model。与在普通表单元素上使用是一样的。 -->
+      <my-channels-vmodel v-model="channel_id_vmodel" :isClearable="true"></my-channels-vmodel>
     </el-form>
   </div>
 </template>
 
 <script>
 import MyChannels from '@/components/MyChannels'
+import MyChannelsVmodel from '@/components/MyChannelsVmodel'
 export default {
   name: 'Home',
-  components: { MyChannels },
+  components: { MyChannels, MyChannelsVmodel },
   data () {
     return {
-      channel_id: ''
+      channel_id: '',
+      channel_id_vmodel: 10
     }
   },
   methods: {
